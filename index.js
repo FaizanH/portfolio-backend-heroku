d2076300dfd83b270344f2f3e678e61a3ffd931c
Faizan <faizanh53@gmail.com>
2024-05-08 19:57:12 +1000

    Added gitlab config

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+const tracer = require('dd-trace').init({
+  hostname: 'datadog-agent',
+  port: 8126
+})
+
 require("dotenv").config()
-const express = require("express");
+const express = require("express")
 const app = express()
-const cors = require("cors");
-const mongoose = require("mongoose");
-const ATLAS_URI = process.env.MONGODB_URI;
-const bodyParser = require('body-parser');
+const cors = require("cors")
+const mongoose = require("mongoose")
+const ATLAS_URI = process.env.MONGODB_URI
+const bodyParser = require('body-parser')
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require('./api/swagger.json');
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require('./api/swagger.json')
 
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
 
 const mongoDBConnection = require('./config/dbConn')
 const PORT = process.env.PORT || 5001
 
-// const router = express.Router();
+// const router = express.Router()
 
 // BEGIN MIDDLEWARE
 // https://public-profile-backend-177cfb33de27.herokuapp.com
@@ -24,19 +29,19 @@ const PORT = process.env.PORT || 5001
 // link to live origin
 // https://faizans-portfolio.onrender.com
 
-app.use(express.json());
+app.use(express.json())
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true
 }))
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json())       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-}));
+}))
 
-app.use(cookieParser("secretcode"));
+app.use(cookieParser("secretcode"))
 app.use(
   session({
     name: "__session",
@@ -45,7 +50,7 @@ app.use(
     saveUninitialized: true,
     // cookie: {maxAge : 60000, secure: false, httpOnly: false }
   })
-);
+)
 
 // END OF MIDDLEWARE
 
@@ -53,20 +58,20 @@ app.use(
 mongoDBConnection()
 
 // Routes
-const blogsRouter = require("./routes/blog_posts");
-const postsRouter = require("./routes/posts");
-const authRouter = require("./routes/auth");
-const userRouter = require("./routes/users");
+const blogsRouter = require("./routes/blog_posts")
+const postsRouter = require("./routes/posts")
+const authRouter = require("./routes/auth")
+const userRouter = require("./routes/users")
 
 app.get("/", (req, res) => {
-  res.send("You have landed on the API server");
-});
-app.use("/blogposts", blogsRouter);
-app.use("/posts", postsRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use('/api/v1', router);
+  res.send("You have landed on the API server")
+})
+app.use("/blogposts", blogsRouter)
+app.use("/posts", postsRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// app.use('/api/v1', router)
 app.use('/users', userRouter)
-app.use('/auth', authRouter);
+app.use('/auth', authRouter)
 
 // Start server
 mongoose.connection.once("open", () => {
