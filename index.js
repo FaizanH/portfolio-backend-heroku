7d0db1e1296b78a5943ebae83578ab83526b8a6e
Faizan <faizanh53@gmail.com>
2024-06-02 19:22:47 +1000

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
     }),
     resave: true,
     saveUninitialized: true,
-    cookie: {maxAge : 60000, secure: false, httpOnly: false }
+    // cookie: {maxAge : 60000, secure: false, httpOnly: false }
   })
 )
 
